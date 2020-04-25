f042c55c2430b032925ab5c50c07ac47bdd7ca98
jsbisht <jagdeepbisht89@gmail.com>
2020-04-25 13:48:53 +0530

    Using xml config with constructor injection

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,11 +1,14 @@
 package com.pluralsight;
 
 import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SpeakerService speakerService = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(speakerService.findAll().get(0).getFirstName());
     }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,15 +1,23 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+        // no arg constructor
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Speaker> findAll() {
         return repository.findAll();
     }
+
 }
